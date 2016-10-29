4dc7a67df1c26f3a6631652fc2935362b42d5907
Nathosky006 <mahalesainath@gmail.com>
2016-10-30 01:59:46 +1100

    Commented @application.route parameters

--- application.py
+++ application.py
@@ -3,10 +3,13 @@ from flask import render_template
 
 application = Flask(__name__)
 
-@application.route('/')
-@application.route('/index')
+#@application.route('/')
+#@application.route('/index')
 def index():
 	return render_template('index.html',title='Home')
+
+def hello():
+	return 'Hello World!'
 
 if __name__ == "__main__":
         application.run()
